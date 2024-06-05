1be6768347a7ab2ab8d5abcc48a3974a930693a1
Liviya <liviyashinimahendramoorthi@gmail.com>
2024-06-05 13:46:27 +0800

    Layout

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use App\Http\Controllers\ManageReportController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KAFAActivityController;
 
+
+Route::get('/', function () {
+    return view('layout');
+});
+
+
+Route::resource('/ManageKafaActivity/admins', KAFAActivityController::class);
 Route::get('/ManageReport/Admin', [ManageReportController::class, 'AdminHomepage']);
 
 
