ced8d62e2bf146d66a2ce6a4ab2b650afa618e7d
Gasoolinx <amrifakhri2002@gmail.com>
2024-06-11 13:12:07 +0800

    update by amri

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,8 +17,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'icnumber',
+        'phonenumber',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -33,15 +35,12 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard1', function () {
+    return view('dashboard1');
+})->middleware(['auth', 'verified'])->name('dashboard1');
+
+Route::get('/dashboard2', function () {
+    return view('dashboard2');
+})->middleware(['auth', 'verified'])->name('dashboard2');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
