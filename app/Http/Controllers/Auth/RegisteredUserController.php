9d70c212d98ddd092085bed211929f8cdf165e31
Gasoolinx <amrifakhri2002@gmail.com>
2024-06-11 13:36:58 +0800

    updated

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,7 +49,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+       // Auth::login($user);
 
         return redirect()->route('login')->with('status', 'Registration successful! Please login.');
     }
